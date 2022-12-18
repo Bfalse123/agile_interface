024d67bd08cd1a345268875761f6bf884725c1c2
Apatcha <s.apatchenko@yandex.ru>
2022-12-18 21:08:16 +0300

    fix: .gitignore deleted

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from data import Data
 if __name__ == "__main__":
 	app = Application() #object of GUI
 	data_main_frame_widget = Data(app.main_frame) #object of widget for demonstating scv data
-	search_main_frame_widget = Search(app.main_frame, data_main_frame_widget) #object of widgets for navigating through scv data	
+	search_main_frame_widget = Search(app.main_frame, data_main_frame_widget) #object of widgets for navigating through scv data
 	
 	app.mainloop() #infinite event loop with response to users actions
